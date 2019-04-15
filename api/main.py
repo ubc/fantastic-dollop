f87b149c0c9074ba43ec1ab727c037d5267752d8
John Hsu <john.hsu@ubc.ca>
2019-04-15 16:02:04 -0700

    ADD intial python logging configuration
    Using python's built-in logging module for now. Looked into the
    structured logging libraries but aren't sure if the increased complexity
    is worth it, might want to revisit once I've done some more complex
    projects using this base.

--- api/main.py
+++ api/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 
+import logging
+import logging.config
+import os
+import yaml # for reading the logging config file
+
+# Configures logging for the whole app, will default to using the settings
+# in `config/logging.yml` which will set logging at DEBUG to stdout. The config
+# file path can be overridden with the environment variable `LOG_CONFIG`.
+configPath = "config/logging.yml"
+envConfigPath = os.getenv("LOG_CONFIG", None)
+if envConfigPath:
+    configPath = envConfigPath
+if os.path.exists(configPath):
+    with open(configPath, 'rt') as f:
+        config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+else:
+    logging.basicConfig(level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    logger.debug("HELLO WORLD 1")
     return {"message": "Hello World"}
